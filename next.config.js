5f304213e8f08827d4c74b86f190c63538d1f682
hanguang254 <725204548@qq.com>
2023-12-13 14:28:27 +0800

    renew

--- next.config.js
+++ next.config.js
@@ -10,7 +10,13 @@ const nextConfig = {
         destination: 'https://mock.apifox.cn/m1/2398938-0-default/api/:path*'
         // destination: 'https://loclhost:3000/api/:path*'
       }]
-    }  
+    } ,
+    exportPathMap: function () {
+      return {
+        '/': { page: '/login' }, // 设置默认打开的页面为 '/home'
+        // 添加其他页面的映射
+      };
+    }, 
 }
 
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
-
+  
   return router.pathname === '/login'?
   (<Component {...pageProps} />):
   ( <Layout>
